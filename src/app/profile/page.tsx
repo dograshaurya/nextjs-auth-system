9b50ae067284c9839f2787a3bc5182a50c996f7b
Shaurya Dogra <dograshaurya@gmail.com>
2025-03-20 13:17:35 +0530

    middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import axios from "axios";
+import React, {useState} from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ProfilePage() {
 
     const router = useRouter();
+    const [data, setData] = useState("nothing");
 
     const logout = async () =>  {
         try {
@@ -17,6 +20,13 @@ export default function ProfilePage() {
 
         }
     }
+
+    const getUserDetails = async () => {
+        const res = await axios.get("/api/users/me");
+        console.log(res.data.data);
+        setData(res.data.data._id);
+    }
+
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-900">
             <div className="w-full max-w-2xl p-8 space-y-6 bg-gray-800 rounded-2xl shadow-lg">
@@ -29,6 +39,7 @@ export default function ProfilePage() {
                 <div className="space-y-4">
                     <div className="flex items-center space-x-4">
                         <div>
+                            <h2 className="text-xl text-white">{data === 'nothing' ? "Nothing": <Link href={`/profile/${data}`}>{data}</Link>}</h2>
                             <h2 className="text-xl text-white">John Doe</h2>
                             <p className="text-gray-400">johndoe@example.com</p>
                         </div>
@@ -40,8 +51,9 @@ export default function ProfilePage() {
                         </p>
                     </div>
                     <button 
+                        onClick={getUserDetails}
                         className="w-full p-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-md">
-                        Edit Profile
+                        View Details
                     </button>
                 </div>
             </div>
